d1e5a8fe7c110e4c00cae2b6a0d532d0007dc550
jeremyje <jeremyje@localhost>
2011-03-20 23:21:07 -0400

    Moved code into web apis.

--- beryl-web/src/org/beryl/web/search/bing/IBingSearchResponseHandler.java
+++
@@ -1,7 +0,0 @@
-package org.beryl.web.search.bing;
-
-public interface IBingSearchResponseHandler
-{
-	void onSearchCompleted(final BingSearchResponse response);
-	void onSearchError(final Exception error);
-}
