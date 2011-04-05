c7eaf43e3db47cb310eb860d64fc7dc137dbc11f
futonredemption <futonredemption@localhost>
2011-04-04 21:04:43 -0400

    Removed more stuff that's not needed.

--- beryl-webapis/src/org/beryl/web/search/google/GoogleLocalSearchResult.java
+++ beryl-webapis/src/org/beryl/web/search/google/GoogleLocalSearchResult.java
@@ -8,8 +8,6 @@ import org.json.JSONObject;
 
 /**
  * http://code.google.com/apis/ajaxsearch/documentation/reference.html#_intro_fonje
- * @author jeremyje
- *
  */
 public class GoogleLocalSearchResult extends GoogleSearchResult
 {
