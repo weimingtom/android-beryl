786b37664ac0d021af08aa47a83bdadba8567d69
futonredemption <futonredemption@localhost>
2011-04-10 19:54:55 -0400

    ViewBinder enhancement.

--- make.cmd
+++ make.cmd
@@ -1,2 +1,3 @@
 call ant
 call xcopy /E /H /Y jar-sources D:\android-development\beryl-examples\webcache\libs
+call xcopy /E /H /Y jar-sources D:\android-development\project-beryl-clean\projects\android\apps\MakeMotivator\libs
