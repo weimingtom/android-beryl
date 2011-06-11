e90d5b4ffab1abdc856cfe7fb1c13ed7fc4bb9af
futonredemption <futonredemption@localhost>
2011-06-11 13:18:48 -0400

    Build 5

--- beryl-core/src/org/beryl/app/RegisterableContract.java
+++ beryl-core/src/org/beryl/app/RegisterableContract.java
@@ -3,6 +3,7 @@ package org.beryl.app;
 /** 
  * This interface is used to denote other interfaces as "contracts" that can be registered in the ContractRegistry.
  * 
- * NOTE: There are no methods in this interface.
+ * NOTE: There are no methods in this interface. And yes I know, this could have been an Annotation.
+ * I didn't want to deal with weird runtime retention problems because of ProGuard.
  */
 public interface RegisterableContract {}
