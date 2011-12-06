791381f6257deb5ffb23d1d439348bcc08dc988b
futonredemption <futonredemption@localhost>
2011-12-06 15:34:03 -0500

    Renaming.

--- beryl/src/org/beryl/io/FroyoDirectoryCompat.java
+++ beryl/src/org/beryl/io/FroyoDirectoryCompat.java
@@ -7,10 +7,57 @@ import android.os.Environment;
 
 class FroyoDirectoryCompat extends CupcakeDirectoryCompat {
 
+	public File getPublicMusic(final boolean autoCreate) {
+		File directory = getExternalStoragePublicDirectoryByType(Environment.DIRECTORY_MUSIC, autoCreate);
+		return directory;
+	}
+
+	public File getPublicPodcasts(final boolean autoCreate) {
+		File directory = getExternalStoragePublicDirectoryByType(Environment.DIRECTORY_PODCASTS, autoCreate);
+		return directory;
+	}
+	
+	public File getPublicRingtones(final boolean autoCreate) {
+		File directory = getExternalStoragePublicDirectoryByType(Environment.DIRECTORY_RINGTONES, autoCreate);
+		return directory;
+	}
+	
+	public File getPublicAlarms(final boolean autoCreate) {
+		File directory = getExternalStoragePublicDirectoryByType(Environment.DIRECTORY_ALARMS, autoCreate);
+		return directory;
+	}
+	
+	public File getPublicNotifications(final boolean autoCreate) {
+		File directory = getExternalStoragePublicDirectoryByType(Environment.DIRECTORY_NOTIFICATIONS, autoCreate);
+		return directory;
+	}
+	
+	public File getPublicPictures(final boolean autoCreate) {
+		File directory = getExternalStoragePublicDirectoryByType(Environment.DIRECTORY_PICTURES, autoCreate);
+		return directory;
+	}
+	
+	public File getPublicMovies(final boolean autoCreate) {
+		File directory = getExternalStoragePublicDirectoryByType(Environment.DIRECTORY_MOVIES, autoCreate);
+		return directory;
+	}
+	
+	public File getPublicDownloads(final boolean autoCreate) {
+		File directory = getExternalStoragePublicDirectoryByType(Environment.DIRECTORY_DOWNLOADS, autoCreate);
+		return directory;
+	}
+	
 	@Override
 	public File getPublicApplication(final Context context) {
 		File directory;
 		directory = context.getExternalFilesDir(null);
+		return directory;
+	}
+	
+	public File getExternalStoragePublicDirectoryByType(final String type, final boolean autoCreate) {
+		File directory = null;
+		directory = Environment.getExternalStoragePublicDirectory(type);
+		attemptAutoCreate(autoCreate, directory);
 		return directory;
 	}
 }
--- beryl/src/org/beryl/io/IDirectoryCompat.java
+++ beryl/src/org/beryl/io/IDirectoryCompat.java
@@ -8,13 +8,13 @@ interface IDirectoryCompat {
 	
 	File getPublicApplication(Context context);
 	File getPublicMusic(final boolean autoCreate);
-	File getPublicPodcast(final boolean autoCreate);
-	File getPublicRingtone(final boolean autoCreate);
-	File getPublicAlarm(final boolean autoCreate);
-	File getPublicNotification(final boolean autoCreate);
-	File getPublicPicture(final boolean autoCreate);
-	File getPublicMovie(final boolean autoCreate);
-	File getPublicDownload(final boolean autoCreate);
+	File getPublicPodcasts(final boolean autoCreate);
+	File getPublicRingtones(final boolean autoCreate);
+	File getPublicAlarms(final boolean autoCreate);
+	File getPublicNotifications(final boolean autoCreate);
+	File getPublicPictures(final boolean autoCreate);
+	File getPublicMovies(final boolean autoCreate);
+	File getPublicDownloads(final boolean autoCreate);
 
 	File getPublicPictureLibrary(final String libraryName);
 }
