ff837e902cb61da9f196deceb8954df7ed86a37d
futonredemption <futonredemption@localhost>
2012-02-16 15:08:08 -0500

    Fixed component toggler bug

--- make.cmd
+++ make.cmd
@@ -6,3 +6,4 @@ xcopy /Y /S /H jar-sources\* %APPDIR%\MyLocationRedux\libs\
 xcopy /Y /S /H jar-sources\* %APPDIR%\VolumeWidget\libs\
 xcopy /Y /S /H jar-sources\* %APPDIR%\SmartSettings\Common\libs\
 xcopy /Y /S /H jar-sources\* %APPDIR%\WidgetPreview\libs\
+xcopy /Y /S /H jar-sources\* %APPDIR%\NoKeyguard\libs\
